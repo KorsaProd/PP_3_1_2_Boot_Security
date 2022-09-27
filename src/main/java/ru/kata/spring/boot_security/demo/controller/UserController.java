ba9ae8217f8770e0c43fb970d13fbf0851ad4ae5
Савелий Коротков <savvakor@gmail.com>
2022-09-27 23:03:32 +0300

    add test admin user

--- src/main/java/ru/kata/spring/boot_security/demo/controller/UserController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/UserController.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import ru.kata.spring.boot_security.demo.model.User;
-import ru.kata.spring.boot_security.demo.service.UserService;
+import ru.kata.spring.boot_security.demo.service.UserServiceImpl;
 
 
 import java.security.Principal;
@@ -14,10 +14,10 @@ import java.security.Principal;
 @RequestMapping("/user")
 public class UserController {
 
-    UserService userService;
+    UserServiceImpl userService;
 
     @Autowired
-    public UserController(UserService userService) {
+    public UserController(UserServiceImpl userService) {
         this.userService = userService;
     }
 
